dc641f5b8b47eca53c31fb6c34cd18ed5da80760
dainedavies31-creator <dainedavies31@gmail.com>
2026-06-10 19:53:22 +0530

    Delete lesson1.js

--- lessons/lesson1.js
+++
@@ -1,8 +0,0 @@
-import { carboncompounds } from "./CarbonCompounds.js";
-document.addEventListener("DOMContentLoaded", init);
-
-function init() {
-  let sim = new carboncompounds(document.getElementById('carboncompoundsbox'));
-  sim.render('ethane');
-  
-};
